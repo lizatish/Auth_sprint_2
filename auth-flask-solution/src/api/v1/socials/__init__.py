12c85c5359107b3b799300b5e825a0c7c45ed705
Oleg <oleg.rubtsov99@gmail.com>
2022-11-09 21:15:20 +0400

    flake8

--- auth-flask-solution/src/api/v1/socials/__init__.py
+++ auth-flask-solution/src/api/v1/socials/__init__.py
@@ -2,4 +2,4 @@ from flask import Blueprint
 
 auth_socials_v1 = Blueprint('auth_socials_v1', __name__)
 
-from . import google, facebook  # noqa: F401    
+from . import google, facebook  # noqa: F401
