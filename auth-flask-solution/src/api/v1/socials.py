3fe6e44001c71541daa7006df903af782f537b42
Elizaveta Tishkovets <etishkovec@ubic.tech>
2022-11-14 00:43:25 +0300

    Fix bug with auth params by socials

--- auth-flask-solution/src/api/v1/socials.py
+++ auth-flask-solution/src/api/v1/socials.py
@@ -23,8 +23,14 @@ socials_auth_mapping = {
 @auth_socials_v1.route('/<social_name>/login')
 def socials_login(social_name: SocialLoginType):
     """Аутентификация через сторонние сервисы."""
+    try:
+        SocialLoginType(social_name)
+    except ValueError:
+        return JsonService.return_social_name_fail(social_name)
+
     client = oauth.create_client(social_name)
     redirect_uri = url_for('auth_socials_v1.socials_auth', social_name=social_name, _external=True)
+
     uri = client.create_authorization_url(redirect_uri)
     client.save_authorize_data(redirect_uri=redirect_uri, **uri)
     return JsonService.return_success_response(url=uri['url'])
@@ -33,12 +39,16 @@ def socials_login(social_name: SocialLoginType):
 @auth_socials_v1.route('/<social_name>/callback')
 def socials_auth(social_name: SocialLoginType):
     """Метод обратного вызова для аутентификации через сторонние сервисы."""
-    prepared = SocialLoginType(social_name)
-    user_data_kwargs = socials_auth_mapping[prepared]()
+    try:
+        prepared = SocialLoginType(social_name)
+    except ValueError:
+        return JsonService.return_social_name_fail(social_name)
 
+    user_data_kwargs = socials_auth_mapping[prepared]()
     account = AuthService.get_user_social(social_id=user_data_kwargs['social_id'], social_name=social_name)
     if not account:
         account = get_auth_service().create_oauth_user(**user_data_kwargs)
+
     access_token, refresh_token = get_auth_service().create_tokens(account.user)
     get_auth_service().add_to_history(account.user)
     return JsonService.return_success_response(access_token=access_token, refresh_token=refresh_token)
